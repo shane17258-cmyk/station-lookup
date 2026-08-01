e2b37500ac55ab50fbdd0a573e156620d575de4c
Shane <shane@example.com>
2026-08-02 00:59:52 +0800

    initial: 4G station lookup webpage

---
+++ data.js
@@ -0,0 +1,705 @@
+﻿const STATION_DATA = [
+    {
+        "id":  "403826",
+        "siteName":  "苗栗雲水瑤S",
+        "siteNameCV":  "苗栗雲水瑤S",
+        "sec":  "",
+        "stations":  [
+                         "苗栗雲水瑤S"
+                     ],
+        "nrBtsId":  "",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "407785",
+        "siteName":  "苗栗瀧淳富居",
+        "siteNameCV":  "苗栗瀧淳富居S",
+        "sec":  "",
+        "stations":  [
+                         "苗栗瀧淳富居S"
+                     ],
+        "nrBtsId":  "",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "411480",
+        "siteName":  "苗栗三角公園",
+        "siteNameCV":  "苗栗中央L17苗栗三角公園",
+        "sec":  "s1/2/3_s4_s5",
+        "stations":  [
+                         "苗栗中央L17苗栗三角公園",
+                         "苗栗中央L17苗栗忠孝龍岡",
+                         "苗栗中央L17阿自倍爾"
+                     ],
+        "nrBtsId":  "4611480",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "412606",
+        "siteName":  "苗栗南勢揚升",
+        "siteNameCV":  "苗栗中央L27苗栗國馨",
+        "sec":  "s1/3_s4/6",
+        "stations":  [
+                         "苗栗中央L27苗栗南勢揚升",
+                         "苗栗中央L27苗栗國馨"
+                     ],
+        "nrBtsId":  "4612606",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "412636",
+        "siteName":  "苗栗消防衛星",
+        "siteNameCV":  "苗栗衛星消防",
+        "sec":  "",
+        "stations":  [
+                         "苗栗衛星消防"
+                     ],
+        "nrBtsId":  "",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "412760",
+        "siteName":  "苗栗台糖宿舍",
+        "siteNameCV":  "苗栗中央L26苗栗聯合學院",
+        "sec":  "s1/2/3/7_s4/5/6",
+        "stations":  [
+                         "苗栗中央L26苗栗台糖宿舍",
+                         "苗栗中央L26苗栗聯合學院"
+                     ],
+        "nrBtsId":  "4612760",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "412782",
+        "siteName":  "苗栗魚市場",
+        "siteNameCV":  "苗栗中央L14苗栗魚市場",
+        "sec":  "s1/2_s4/5/6",
+        "stations":  [
+                         "苗栗中央L14苗栗魚市場",
+                         "苗栗中央L14苗栗民族"
+                     ],
+        "nrBtsId":  "4612782",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413009",
+        "siteName":  "苗栗三山社區",
+        "siteNameCV":  "苗栗中央L28苗栗三山社區",
+        "sec":  "s1/2/3_s4/5/6_s7",
+        "stations":  [
+                         "苗栗中央L28苗栗三山社區",
+                         "苗栗中央L28苗栗高苗大千",
+                         "苗栗中央L28苗栗大千碧英"
+                     ],
+        "nrBtsId":  "4613009",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413010",
+        "siteName":  "後龍禽宰場",
+        "siteNameCV":  "苗栗中央L29苗栗山海樂活",
+        "sec":  "s1/2_s4_s5_s6",
+        "stations":  [
+                         "苗栗中央L29後龍禽宰場",
+                         "苗栗中央L29苗栗經國至公",
+                         "苗栗中央L29苗栗樂活山莊",
+                         "苗栗中央L29苗栗山海樂活"
+                     ],
+        "nrBtsId":  "4613010",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413015",
+        "siteName":  "苗栗文峯",
+        "siteNameCV":  "苗栗中央L36苗栗文峰",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L36苗栗文峰",
+                         "苗栗中央L36苗栗府前至公"
+                     ],
+        "nrBtsId":  "4613015",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413071",
+        "siteName":  "苗積電封測場",
+        "siteNameCV":  "苗積電測試台",
+        "sec":  "s1",
+        "stations":  [
+                         "苗積電測試台"
+                     ],
+        "nrBtsId":  "",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "413100",
+        "siteName":  "苗栗文觀局",
+        "siteNameCV":  "苗栗中央L41苗栗文觀局",
+        "sec":  "s1/2/3",
+        "stations":  [
+                         "苗栗中央L41苗栗文觀局"
+                     ],
+        "nrBtsId":  "4613100",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413487",
+        "siteName":  "苗栗嘉福",
+        "siteNameCV":  "苗栗嘉盛L08苗栗嘉福",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗嘉盛L08苗栗嘉福",
+                         "苗栗嘉盛L08苗栗嘉惠新村"
+                     ],
+        "nrBtsId":  "4613487",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413713",
+        "siteName":  "苗栗線路中心",
+        "siteNameCV":  "苗栗中央L03苗栗地院",
+        "sec":  "s1/2/3_s4/5_s6",
+        "stations":  [
+                         "苗栗中央L03苗栗線路中心",
+                         "苗栗中央L03苗栗地院",
+                         "苗栗中央L03苗栗地檢署"
+                     ],
+        "nrBtsId":  "4613713",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413731",
+        "siteName":  "苗栗嘉盛",
+        "siteNameCV":  "苗栗嘉盛L01苗栗為公",
+        "sec":  "s1/2/3/8_s4/5/6/7",
+        "stations":  [
+                         "苗栗嘉盛L01苗栗嘉盛",
+                         "苗栗嘉盛L01苗栗為公"
+                     ],
+        "nrBtsId":  "4613731",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413736",
+        "siteName":  "苗栗南苗",
+        "siteNameCV":  "苗栗中央L04苗栗新東",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L04苗栗南苗",
+                         "苗栗中央L04苗栗新東"
+                     ],
+        "nrBtsId":  "4613736",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413738",
+        "siteName":  "苗栗龍岡",
+        "siteNameCV":  "苗栗中央L10苗栗西山",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L10苗栗龍岡",
+                         "苗栗中央L10苗栗西山"
+                     ],
+        "nrBtsId":  "4613738",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413766",
+        "siteName":  "苗栗南勢大千",
+        "siteNameCV":  "苗栗中央L01苗栗恭敬",
+        "sec":  "s1/2/3_s4/5/6/7",
+        "stations":  [
+                         "苗栗中央L01苗栗南勢大千",
+                         "苗栗中央L01苗栗恭敬"
+                     ],
+        "nrBtsId":  "4613766",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413799",
+        "siteName":  "苗栗栗華",
+        "siteNameCV":  "苗栗嘉盛L10苗栗栗華",
+        "sec":  "s1/2/3/4",
+        "stations":  [
+                         "苗栗嘉盛L10苗栗栗華"
+                     ],
+        "nrBtsId":  "4613799",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413802",
+        "siteName":  "苗栗坪頂西",
+        "siteNameCV":  "苗栗中央L20苗栗新勝",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L20苗栗坪頂西",
+                         "苗栗中央L20苗栗新勝"
+                     ],
+        "nrBtsId":  "4613802",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413820",
+        "siteName":  "苗栗鳳形",
+        "siteNameCV":  "苗栗中央L13苗栗鳳形",
+        "sec":  "s1/2/3_s4",
+        "stations":  [
+                         "苗栗中央L13苗栗鳳形",
+                         "苗栗中央L13西湖半天寮RH1"
+                     ],
+        "nrBtsId":  "4613820",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413821",
+        "siteName":  "苗栗新東橋",
+        "siteNameCV":  "苗栗中央L05苗栗新東橋",
+        "sec":  "s1/2/3/7_s4/5/6",
+        "stations":  [
+                         "苗栗中央L05苗栗新東橋",
+                         "苗栗中央L05苗栗金華"
+                     ],
+        "nrBtsId":  "4613821",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413835",
+        "siteName":  "苗栗南勢聯大BBU1",
+        "siteNameCV":  "南勢聯大機械系RH1",
+        "sec":  "s1_s2_s3",
+        "stations":  [
+                         "南勢聯大機械系RH1",
+                         "南勢聯大機械系RH2",
+                         "苗栗南勢聯大餐廳RH3"
+                     ],
+        "nrBtsId":  "4613835",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "413840",
+        "siteName":  "苗栗嘉新福樂",
+        "siteNameCV":  "苗栗嘉盛L06經國河濱",
+        "sec":  "s1/2/3_s4",
+        "stations":  [
+                         "苗栗嘉盛L06苗栗嘉新福樂",
+                         "苗栗嘉盛L06經國河濱"
+                     ],
+        "nrBtsId":  "4613840",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "413970",
+        "siteName":  "苗栗社寮",
+        "siteNameCV":  "苗栗嘉盛L05苗栗社寮",
+        "sec":  "s1/3_s4/5/6",
+        "stations":  [
+                         "苗栗嘉盛L05苗栗社寮",
+                         "苗栗嘉盛L05苗栗中華"
+                     ],
+        "nrBtsId":  "4613970",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "414006",
+        "siteName":  "苗栗南勢坑",
+        "siteNameCV":  "苗栗中央L06苗栗松園",
+        "sec":  "s1/2/3_s4/6",
+        "stations":  [
+                         "苗栗中央L06苗栗南勢坑",
+                         "苗栗中央L06苗栗松園"
+                     ],
+        "nrBtsId":  "4614006",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "415411",
+        "siteName":  "苗栗香榭",
+        "siteNameCV":  "苗栗嘉盛L09苗栗香榭",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗嘉盛L09苗栗香榭",
+                         "苗栗嘉盛L09苗栗福安"
+                     ],
+        "nrBtsId":  "4615411",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "415421",
+        "siteName":  "苗栗聯合大學",
+        "siteNameCV":  "苗栗聯合大學",
+        "sec":  "s1/2/3",
+        "stations":  [
+                         "苗栗聯合大學"
+                     ],
+        "nrBtsId":  "4615421",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "415426",
+        "siteName":  "苗栗衛署醫院",
+        "siteNameCV":  "苗栗嘉盛L02苗栗五谷",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗嘉盛L02苗栗衛署醫院",
+                         "苗栗嘉盛L02苗栗五谷"
+                     ],
+        "nrBtsId":  "4615426",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "415427",
+        "siteName":  "苗栗長春",
+        "siteNameCV":  "苗栗中央L40苗栗長春",
+        "sec":  "s2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L40苗栗長春",
+                         "苗栗中央L40苗栗新亞拌合"
+                     ],
+        "nrBtsId":  "4615427",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "415428",
+        "siteName":  "苗栗福星",
+        "siteNameCV":  "苗栗嘉盛L04苗栗福星",
+        "sec":  "s1/2/3",
+        "stations":  [
+                         "苗栗嘉盛L04苗栗福星"
+                     ],
+        "nrBtsId":  "4615428",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "415429",
+        "siteName":  "苗栗巨蛋",
+        "siteNameCV":  "苗栗中央L18苗栗龍岡二",
+        "sec":  "s1/2/3_s5/6",
+        "stations":  [
+                         "苗栗中央L18苗栗巨蛋",
+                         "苗栗中央L18苗栗龍岡二"
+                     ],
+        "nrBtsId":  "4615429",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "415448",
+        "siteName":  "苗栗上苗",
+        "siteNameCV":  "苗栗嘉盛L03苗栗上苗",
+        "sec":  "s2/3",
+        "stations":  [
+                         "苗栗嘉盛L03苗栗上苗"
+                     ],
+        "nrBtsId":  "4615448",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "416914",
+        "siteName":  "苗栗苗栗BBU7",
+        "siteNameCV":  "苗栗育民RH1",
+        "sec":  "s1_s2_s3",
+        "stations":  [
+                         "苗栗育民RH1",
+                         "苗栗育民RH2",
+                         "輝生加油站RH3"
+                     ],
+        "nrBtsId":  "",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "416922",
+        "siteName":  "苗栗西勢美北",
+        "siteNameCV":  "苗栗中央L07苗栗維祥",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L07苗栗西勢美北",
+                         "苗栗中央L07苗栗維祥"
+                     ],
+        "nrBtsId":  "4616922",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "416935",
+        "siteName":  "苗栗苗栗BBU6",
+        "siteNameCV":  "苗栗中央L22苗龍幹32-1",
+        "sec":  "s1_s2_s3_s4_s5_s6_s7",
+        "stations":  [
+                         "苗栗中央L22苗栗杜石地",
+                         "苗栗中央L22苗栗南勢台鐵",
+                         "苗栗中央L22苗栗南勢忠愛",
+                         "苗栗中央L22苗栗大將軍",
+                         "苗栗中央L22晉陞太空",
+                         "苗栗中央L22苗栗心衛中心",
+                         "苗栗中央L22苗龍幹32-1"
+                     ],
+        "nrBtsId":  "4616935",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "416938",
+        "siteName":  "苗栗聯大BBU1",
+        "siteNameCV":  "苗栗聯大綜合大樓RH2",
+        "sec":  "s1/2_s3",
+        "stations":  [
+                         "苗栗聯大綜合大樓RH1",
+                         "苗栗聯大女一宿RH3"
+                     ],
+        "nrBtsId":  "4616938",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "417108",
+        "siteName":  "苗栗復興",
+        "siteNameCV":  "苗栗中央L38苗栗復興",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L38苗栗復興",
+                         "苗栗中央L38苗栗市農會"
+                     ],
+        "nrBtsId":  "4617108",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417122",
+        "siteName":  "苗栗玉維",
+        "siteNameCV":  "苗栗中央L24苗栗市廣",
+        "sec":  "s1/2/3_s5_s6_s7",
+        "stations":  [
+                         "苗栗中央L24苗栗玉維",
+                         "苗栗中央L24苗栗市廣",
+                         "苗栗中央L24苗栗垃圾場",
+                         "苗栗中央L24苗栗雲水瑤"
+                     ],
+        "nrBtsId":  "4617122",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417126",
+        "siteName":  "苗栗南勢聯大",
+        "siteNameCV":  "苗栗南勢聯大",
+        "sec":  "s1/2/3",
+        "stations":  [
+                         "苗栗南勢聯大"
+                     ],
+        "nrBtsId":  "4617126",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "417146",
+        "siteName":  "苗栗水源",
+        "siteNameCV":  "苗栗中央L02苗栗吉祥",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L02苗栗水源",
+                         "苗栗中央L02苗栗吉祥"
+                     ],
+        "nrBtsId":  "4617146",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417221",
+        "siteName":  "苗栗玉華玉清",
+        "siteNameCV":  "苗栗中央L08苗栗玉華玉清",
+        "sec":  "s1/2/3",
+        "stations":  [
+                         "苗栗中央L08苗栗玉華玉清"
+                     ],
+        "nrBtsId":  "4617221",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417630",
+        "siteName":  "苗栗中央L31",
+        "siteNameCV":  "苗栗中央L31苗栗維新里S",
+        "sec":  "s1_s2_s3",
+        "stations":  [
+                         "苗栗中央L31苗栗維新里S",
+                         "苗栗中央L31苗栗府前門市MR",
+                         "苗栗中央L31苗栗中正門市MR"
+                     ],
+        "nrBtsId":  "4617630",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417735",
+        "siteName":  "苗栗文發",
+        "siteNameCV":  "苗栗中央L12苗栗英明",
+        "sec":  "s1/2/3_s4/5/6/7",
+        "stations":  [
+                         "苗栗中央L12苗栗文發",
+                         "苗栗中央L12苗栗英明"
+                     ],
+        "nrBtsId":  "4617735",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417773",
+        "siteName":  "苗栗大明寺",
+        "siteNameCV":  "苗栗中央L37苗栗大明寺",
+        "sec":  "s2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L37苗栗大明寺",
+                         "苗栗中央L37苗栗僑育"
+                     ],
+        "nrBtsId":  "4617773",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417777",
+        "siteName":  "苗栗台隧苗南",
+        "siteNameCV":  "苗栗中央L32苗栗台隧苗南",
+        "sec":  "s1_s4",
+        "stations":  [
+                         "苗栗中央L32苗栗台隧苗南",
+                         "苗栗中央L32南勢車站"
+                     ],
+        "nrBtsId":  "4617777",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417785",
+        "siteName":  "苗栗正發",
+        "siteNameCV":  "苗栗正發",
+        "sec":  "s1/2/3",
+        "stations":  [
+                         "苗栗正發"
+                     ],
+        "nrBtsId":  "4617785",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "417786",
+        "siteName":  "苗栗國華",
+        "siteNameCV":  "苗栗中央L11苗栗家樂福",
+        "sec":  "s1/2/3_s4/5/6/7",
+        "stations":  [
+                         "苗栗中央L11苗栗國華",
+                         "苗栗中央L11苗栗家樂福"
+                     ],
+        "nrBtsId":  "4617786",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417823",
+        "siteName":  "苗栗苗栗BBU8",
+        "siteNameCV":  "苗栗中央L09苗栗中油探採",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L09苗栗中油探採",
+                         "苗栗中央L09苗栗長春二廠"
+                     ],
+        "nrBtsId":  "4617823",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417966",
+        "siteName":  "苗栗玉清台銀",
+        "siteNameCV":  "苗栗中央L19苗栗玉清台銀",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L19苗栗玉清台銀",
+                         "苗栗中央L19苗栗府前新"
+                     ],
+        "nrBtsId":  "4617966",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "417969",
+        "siteName":  "苗栗苗栗BBU9",
+        "siteNameCV":  "苗栗府前三RH",
+        "sec":  "s1_s2_s4",
+        "stations":  [
+                         "苗栗府前一RH1",
+                         "苗栗府前二RH1",
+                         "苗栗府前三RH"
+                     ],
+        "nrBtsId":  "4617969",
+        "ranType":  "D-RAN"
+    },
+    {
+        "id":  "418965",
+        "siteName":  "苗栗嘉盛L15",
+        "siteNameCV":  "苗栗嘉盛L15苗栗信邦電子",
+        "sec":  "s1_s2_s3_s4_s6",
+        "stations":  [
+                         "苗栗嘉盛L15苗栗玉清里S",
+                         "苗栗嘉盛L15苗栗福星里S",
+                         "苗栗嘉盛L15苗栗坡塘巷",
+                         "苗栗嘉盛L15苗栗信邦電子",
+                         "苗栗嘉盛L15苗栗宜春路"
+                     ],
+        "nrBtsId":  "4618965",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "419548",
+        "siteName":  "苗栗台肥",
+        "siteNameCV":  "苗栗嘉盛L07苗栗消防局",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗嘉盛L07苗栗台肥",
+                         "苗栗嘉盛L07苗栗消防局"
+                     ],
+        "nrBtsId":  "4619548",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "423730",
+        "siteName":  "苗栗中央L25",
+        "siteNameCV":  "苗栗中央L25線路中心內",
+        "sec":  "s1/2/3/4_s5/6",
+        "stations":  [
+                         "苗栗中央L25苗栗苗栗",
+                         "苗栗中央L25線路中心內"
+                     ],
+        "nrBtsId":  "4623730",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "424000",
+        "siteName":  "苗栗中央L34",
+        "siteNameCV":  "苗栗中央L34苗栗福麗",
+        "sec":  "s1/2/3_s5/6",
+        "stations":  [
+                         "苗栗中央L34苗栗福麗",
+                         "苗栗中央L34苗栗宏苗大樓"
+                     ],
+        "nrBtsId":  "4624000",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "424060",
+        "siteName":  "苗栗中央L33",
+        "siteNameCV":  "苗栗中央L33苗栗建功",
+        "sec":  "s1/2/3_s4/5/6/7",
+        "stations":  [
+                         "苗栗中央L33苗栗營業中心",
+                         "苗栗中央L33苗栗建功"
+                     ],
+        "nrBtsId":  "4617630_4624060",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "427202",
+        "siteName":  "苗栗中央L39",
+        "siteNameCV":  "苗栗中央L39苗栗中龍岡",
+        "sec":  "s1/2_s4/5",
+        "stations":  [
+                         "苗栗中央L39苗栗電台",
+                         "苗栗中央L39苗栗中龍岡"
+                     ],
+        "nrBtsId":  "4627202",
+        "ranType":  "C-RAN"
+    },
+    {
+        "id":  "427678",
+        "siteName":  "苗栗中央L35",
+        "siteNameCV":  "苗栗中央L35苗栗國稅局",
+        "sec":  "s1/2/3_s4/5/6",
+        "stations":  [
+                         "苗栗中央L35苗栗國稅局",
+                         "苗栗中央L35苗栗皇家豪門"
+                     ],
+        "nrBtsId":  "4627678",
+        "ranType":  "C-RAN"
+    }
+];
